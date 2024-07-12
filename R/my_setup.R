b1e196fc1a2dacfcd0693d38fa3946a636be2ef3
James Bartlett <james.bartlett@glasgow.ac.uk>
2024-07-12 12:36:31 +0100

    Draft of C02 until introducing R Markdown

--- R/my_setup.R
+++ R/my_setup.R
@@ -17,3 +17,30 @@ if (requireNamespace("knitr", quietly = TRUE)) {
     dpi = 96
   )
 }
+
+# Code from Lisa to help with table formatting as Quarto does not currently work well with kableExtra
+
+glossary_table <- function(as_kable = TRUE) {
+  glossary <- glossary_options("table")
+  if (is.null(glossary))
+    glossary <- list()
+  term <- names(glossary)
+  linked_term <- term
+  if (!is.null(glossary_path()) && glossary_path() == "psyteachr") {
+    lcterm <- gsub(" ", "-", tolower(term), fixed = TRUE)
+    first_letter <- substr(lcterm, 1, 1)
+    linked_term <- paste0(" [", lcterm, "](https://psyteachr.github.io/glossary/",
+                          first_letter, "#", lcterm, "){target='_blank' class='glossary'} ")
+  }
+  if (is.null(term)) {
+    data.frame()
+  } else if (as_kable) {
+    the_list <- data.frame(term = linked_term, definition = unlist(glossary))
+    kable(the_list[order(term), ],
+          escape = FALSE,
+          row.names = FALSE, )
+  } else {
+    the_list <- data.frame(term = term, definition = unlist(glossary))
+    the_list[order(term), ]
+  }
+}
